27759c416b33c4e3f04217a449d979871962af16
divya-patil-CS <divyaoffcial33@gmail.com>
2026-01-04 18:46:04 +0530

    Create variables_data_types.py

---
+++ variables_data_types.py
@@ -0,0 +1,19 @@
+name = "Divya"
+age = 18
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+marks = 89.7
+marks_int = int(marks)
+print("Marks (float):", marks)
+print("Marks (int):", marks_int)
