12d7c611d17113da5c718062cd52b229e0beb0d1
divya-patil-CS <divyaoffcial33@gmail.com>
2026-01-04 18:48:15 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,16 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+
+# Arithmetic operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+
+
+# Comparison operators
+print("a == b:", a == b)
+print("a > b:", a > b)
+print("a < b:", a < b)
